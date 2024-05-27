19cfb900a81781ac79b18db7af9d1e3b058e316b
Ildar <iledar@icloud.com>
2024-05-28 01:56:44 +0500

    Добавляю правки (#13)
    * Удаляю класс у main
    
    * Дабавляет класс для main
    
    * Менят js
    
    * Меняет index.js
    
    * Добавляет правки
    
    * Исправляет фоновые цвета
    * Добавляет изображения карты есле js не сработал
    * Убирает кнопку в навигации если оы не сработал
    
    * Добавляет правки

--- source/scripts/map.js
+++ source/scripts/map.js
@@ -2,23 +2,42 @@ window.addEventListener('pageshow', (event) => {
   if (!event.persisted) {
     // Страница не из bfcache, загружаем iframe
     setTimeout(() => {
+      const mapPlaceholder = document.getElementById('map-placeholder');
       const iframe = document.createElement('iframe');
+      iframe.className = 'map__frame';
       iframe.src = 'https://yandex.ru/map-widget/v1/?um=constructor%3A1e01fcb9e2d8c07c526bb53341fe31b72de6b512a0f40072987b74fc0b1c7dca&source=constructor';
       iframe.width = '1440';
       iframe.height = '400';
       iframe.allowFullscreen = true;
       iframe.loading = 'lazy';
       iframe.title = 'Карта с нашим местоположением';
-      iframe.className = 'map__frame';
-      document.getElementById('map-placeholder').appendChild(iframe);
+      mapPlaceholder.appendChild(iframe);
+
+      // Обработчик успешной загрузки iframe
+      iframe.onload = () => {
+        // Скрываем запасное изображение
+        const mapFallback = document.querySelector('.map__fallback');
+        mapFallback.style.display = 'none';
+      };
 
       // Обработчик ошибки загрузки iframe
-      iframe.addEventListener('error', () => {
-        // Получаем элемент .map__image
+      iframe.onerror = () => {
+        // Отображаем запасное изображение
+        const mapFallback = document.querySelector('.map__fallback');
+        mapFallback.style.display = 'block';
+        // Устанавливаем свойство display на inline-block для .map__image
         const mapImage = document.querySelector('.map__image');
-        // Устанавливаем свойство display на block
-        mapImage.style.display = 'block';
-      });
+        mapImage.style.display = 'inline-block';
+      };
     }, 3000);
+  }
+});
+
+window.addEventListener('load', () => {
+  // Проверяем, загружен ли iframe
+  const iframe = document.querySelector('.map__frame');
+  if (iframe && iframe.contentWindow && iframe.contentWindow.document) {
+    // Если iframe загружен, скрываем запасное изображение
+    document.querySelector('.map__fallback').style.display = 'none';
   }
 });
